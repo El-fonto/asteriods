f8634623a9daa97460eb6edf041ab3ab16b18324
Fonto <laotrafuente880@gmail.com>
2024-10-26 10:45:48 -0600

    player, circle shapes and triangle rendered added

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 import pygame
 from constants import *
+from circleshape import CircleShape
+from player import Player
 
 def main():
     pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    # color = pygame.Surface.map_rgb(0,0,0)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player( SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+    
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
 
         clock.tick(60)
         dt = clock.tick(60) / 1000
+
 
 
 
