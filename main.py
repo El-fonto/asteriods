6a522efd8490f28f945eb8b5cf40698ef43eafa9
El-fonto <laotrafuente880@gmail.com>
2024-10-26 07:54:34 -0600

    clock ticking to less than 60 fps added

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +20,10 @@ def main():
 
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
