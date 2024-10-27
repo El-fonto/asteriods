d2c985ee43dddcbe70a6a919bc3f432767f30562
El-fonto <laotrafuente880@gmail.com>
2024-10-27 11:41:57 -0600

    asteroid object created, draft of implementation, cleaned first print

--- main.py
+++ main.py
@@ -2,24 +2,30 @@ import pygame
 from constants import *
 from circleshape import CircleShape
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
-    # two groups
+    # static groups
+    asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
 
+    # containers
     Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
 
-    player = Player( SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = AsteroidField()
+    asteroid = Asteroid()
+    player = Player (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
     dt = 0
+    clock = pygame.time.Clock()
 
 
     while True:
